76afaf8513c6101823834a7d38d6b0ce31439c97
Erik Kallen <erikkallen@incas3.eu>
2016-04-29 22:48:04 +0200

    bla

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include "mqtt.h"
 #include <stdio.h>
+#include <yaml-cpp/yaml.h>
 
 int main(int argc, char const *argv[]) {
     /* code */
+
+    YAML::Node config = YAML::LoadFile("configuration.yaml");
+
+    if (config["lastLogin"]) {
+      std::cout << "Last logged in: " << config["lastLogin"].as<DateTime>() << "\n";
+    }
+
+    const std::string username = config["username"].as<std::string>();
+    const std::string password = config["password"].as<std::string>();
+
 
     MQTT * mqttHdl;
     mqttHdl = new MQTT(NULL,"livingroom/light/switch/set", "10.0.0.21", 1883);
